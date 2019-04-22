a26b9464c3cc49b770f02527db966cc3c610bea2
Daemon <gina.brian@163.com>
2019-04-22 15:50:44 +0800

    debug

--- ytb2.py
+++ ytb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import requests as r
 import concurrent.futures
-import json,time,random,mimetypes,sys
+import json,time,random,mimetypes,sys,threading
 from urllib.parse import urlparse,parse_qs,unquote
 
 def get_video_size(url):
@@ -10,6 +10,7 @@ def get_video_size(url):
     content_length = info.headers.get('content-length') 
     m['size'] = int(content_length)
     m['mimetype'] = info.headers.get('content-type')
+    m['rangeable'] = info.headers.get('accept-ranges')
     return m
 
 def find_max_quality_url(urls):
@@ -34,12 +35,14 @@ def find_max_quality_url(urls):
                 max_f["url"] = url
                 max_f["size"] = size
                 max_f['mimetype'] = mime_type
+                max_f['rangeable'] = m['rangeable']
                 continue
             
             if max_f.get("size") < size:
                 max_f["url"] = url
                 max_f["size"] = size
                 max_f['mimetype'] = mime_type
+                max_f['rangeable'] = m['rangeable']
     return max_f                
 
 def get_filename(mime_type):
@@ -59,6 +62,38 @@ def get_filename(mime_type):
     filename = '{}{}'.format(title or vid,mime_type)
     return filename
 
+def download(info):
+    size = info.get('size')
+    thread_num = 1 if size < 10485760 else  4
+    partital_size =  size / thread_num
+    ranges = []
+    if thread_num > 1 and info.get('rangeable'):
+        for i in range(0,thread_num):
+            s = i * partital_size
+            e = (i + 1) * partital_size if i + 1 < 4 else size
+            ranges.append("{}-{}".format(s,e))
+    if len(ranges) == 0:
+        filenames = [vid]
+    else:
+        filenames = ["{}-{}".format(vid,range) for range in ranges]
+
+    lock = threading.Lock()
+    sum = 0
+    def _download(url,filename):
+        res = r.get(url,stream=True,headers={'Range':"bytes={}".format(range)})
+        with open(filename, 'wb') as fd:
+            for chunk in res.iter_content(chunk_size=65535):
+               with lock:
+                    sum += 65535
+                fd.write(chunk)
+                sys.stdout.write('\r {:.2f}%'.format(sum * 100 / cl))
+                sys.stdout.flush()
+            fd.flush()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=thread_num) as executor:
+        futures = [executor.submit(_download,url,filename) for filename in filenames]
+        concurrent.futures.wait(future_to_url)
+    return filenames
+
 
 def main():
     global content 
@@ -74,14 +109,21 @@ def main():
     if not adaptive_fmts_encoded or len(adaptive_fmts_encoded) == 0:
         print("get video info fail")
         exit(-1)
-    #filename = get_filename()  
     video_urls = parse_qs(adaptive_fmts_encoded[0]).get("url")
-    # for video_url in map(unquote,video_urls):
-        # print(video_url)  
     max_f = find_max_quality_url(video_urls)
-    filename = get_filename(max_f.get('mimetype'))
+    videoname = get_filename(max_f.get('mimetype'))
     print(max_f)
-    print(filename)
+    print(videoname)
+    filenames = download(max_f)
+    with open(videoname,"wb") as out:
+        for filename in filenames:
+            with open(filename,"rb") as fin:
+                content = memoryview(fin.read())
+                out.write(content)
+            out.flush()
+    print("done!")                
+
+
 
 if __name__ == "__main__":
     main()
